810c2e809db4e894b953efff32cfcc677c5fb2f9
sadiqchigur <sadiq.chigur0391@gmail.com>
2025-11-17 18:33:42 +0530

    Clean up comments in App.java
    Removed commented-out code from App.java

--- example/App.java
+++ example/App.java
@@ -2,8 +2,6 @@ package com.example;
 
 public class App {
     /// Testing .... ... 
-    // saodijfon 
-    // eryettyjf srht etr rjyert ...  .. 
     public static void main(String[] args) {
         System.out.println(Library.greet());
     }
