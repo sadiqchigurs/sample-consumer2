c7ccb07bb0050a9195c7639b427017f08b5ef5fa
sadiqchigur <sadiq.chigur0391@gmail.com>
2025-11-17 12:32:58 +0530

    Add comments in App.java for testing
    Added comments for testing purposes.

--- example/App.java
+++ example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
 public class App {
+    /// Testing .... ... 
+    // saodijfon 
     public static void main(String[] args) {
         System.out.println(Library.greet());
     }
